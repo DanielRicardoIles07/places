ef5ca11ba56a60b781cd4b54236ccb5895ec277e
Danie Ricardo Iles <danieliles18@gmail.com>
2017-12-07 14:48:19 -0500

    update curso 1 completo

---
+++ src/requests/places.js
@@ -0,0 +1,19 @@
+export default {
+  places: [
+    {
+        imageUrl: '/images/place-food-1.jpg',
+        title: 'Place 1',
+        description: 'This is the places number one'
+    },
+    {
+        imageUrl: '/images/place-food-1.jpg',
+        title: 'Place 2',
+        description: 'This is the places number two'
+    },
+    {
+        imageUrl: '/images/place-food-1.jpg',
+        title: 'Place 3',
+        description: 'This is the places number three'
+    }
+  ]
+}
