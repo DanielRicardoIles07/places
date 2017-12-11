6dc659888b42fef2b1509891fe062aa1afd7e947
Danie Ricardo Iles <danieliles18@gmail.com>
2017-12-11 11:47:52 -0500

    curso 2, video 4.3 componentes de la animacion

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TransitionGroup from 'react-transition-group/TransitionGroup';
 import CSSTransition from 'react-transition-group/CSSTransition';
-import {withRouter} from 'react-router-dom';
-
-import  './App.css';
+import { withRouter } from 'react-router-dom';
 
 import MyAppBar from './components/navigation/MyAppBar';
 
+import  './App.css';
 
 class App extends Component {
 
   constructor(props){
     super(props);
+    console.log(props);
   }
 
 
@@ -20,12 +20,14 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <MyAppBar/>
-        <TransitionGroup>
-          <CSSTransition classNames="left-out" timeout={300} key={this.props.location.key}>
-            {this.props.children}
-          </CSSTransition>
-        </TransitionGroup>
+        <div>
+          <MyAppBar/>
+          <TransitionGroup>
+            <CSSTransition classNames="left-out" timeout={300} mountOnEnter={true} unmountOnExit={true} key={this.props.location.pathname.split('/')[1]}>
+              {this.props.children}
+            </CSSTransition>
+          </TransitionGroup>
+        </div>
       </MuiThemeProvider>
     );
   }
--- src/requests/places.js
+++ src/requests/places.js
@@ -1,19 +1,19 @@
 export default {
   places: [
     {
-        imageUrl: '/images/place-food-1.png',
+        imageUrl: '/images/place-food-1.jpg',
         title: 'Place 1',
         description: 'This is the places number one',
         address: 'Cra 7 #156-30'
     },
     {
-        imageUrl: '/images/place-food-1.png',
+        imageUrl: '/images/place-food-1.jpg',
         title: 'Place 2',
         description: 'This is the places number two',
         address: 'Cra 7 #156-30'
     },
     {
-        imageUrl: '/images/place-food-1.png',
+        imageUrl: '/images/place-food-1.jpg',
         title: 'Place 3',
         description: 'This is the places number three',
         address: 'Cra 7 #156-30'
